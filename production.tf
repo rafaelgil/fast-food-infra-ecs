e89c892344cbe33b9247d5d10f88d3b95479c77e
Rafael Carmona Gil <rgil@uolinc.com>
2023-10-30 18:14:21 -0300

    Pipeline fast food

--- production.tf
+++ production.tf
@@ -13,15 +13,15 @@ provider "aws" {
 module "ecs" {
   source             = "./ecs"
   environment        = local.environment
-  vpc_id             = "vpc-0932da4df69a1e2b5"
+  vpc_id             = "vpc-07c0309e03c1c0b61"
   availability_zones = local.production_availability_zones
   repository_name    = "fast-food-app"
-  subnets_ids        = ["subnet-0b8b700720d03c613", "subnet-05cf28d4e5aff2547"]
-  public_subnet_ids  = ["subnet-0d4c9688a70c390b8", "subnet-02cec4a001d01f990"]
+  subnets_ids        = ["subnet-06f742412121a2c42", "subnet-0bd1ea15b26a2c6d9"]
+  public_subnet_ids  = ["subnet-0669d393efec667b6", "subnet-07611e21c22349b2b"]
   security_groups_ids = [
-    "sg-00cc419a06cf9c8de", "sg-081cb7d0daa2a8211"
+    "sg-0c6296cdd76713cd3", "sg-06191e3cec3a38f00"
   ]
-  database_endpoint = ""
+  database_endpoint = "fast-food-database.csxw4cuf3uvj.us-east-1.rds.amazonaws.com"
   database_name     = var.production_database_name
   database_username = var.production_database_username
   database_password = var.production_database_password
